46bd6086b946f5556d2be750758e2618f2e9f68b
Justus Winter <justus@g10code.com>
2017-07-18 14:24:47 +0200

    jenkins: fix running tests on windows

--- misc/jenkins/bin/run-tests.bat
+++ misc/jenkins/bin/run-tests.bat
@@ -21,7 +21,7 @@ cd /d %WD%
 mkdir openpgp
 cd openpgp
 
-set srcdir=%BIN_PREFIX%/tests/openpgp
+set abs_top_srcdir=%BIN_PREFIX%/tests/openpgp
 %BIN_PREFIX%\gpgscm.exe %srcdir%/run-tests.scm --parallel version.scm enarmor.scm mds.scm decrypt.scm decrypt-multifile.scm decrypt-dsa.scm decrypt-session-key.scm sigs.scm sigs-dsa.scm encrypt.scm encrypt-multifile.scm encrypt-dsa.scm compression.scm seat.scm clearsig.scm encryptp.scm detach.scm detachm.scm armsigs.scm armencrypt.scm armencryptp.scm signencrypt.scm signencrypt-dsa.scm armsignencrypt.scm armdetach.scm armdetachm.scm genkey1024.scm conventional.scm conventional-mdc.scm multisig.scm verify.scm verify-multifile.scm gpgv-forged-keyring.scm armor.scm import.scm import-revocation-certificate.scm ecc.scm 4gb-packet.scm tofu.scm use-exact-key.scm default-key.scm export.scm ssh-import.scm ssh-export.scm quick-key-manipulation.scm key-selection.scm delete-keys.scm gpgconf.scm issue2015.scm issue2346.scm issue2417.scm issue2419.scm issue2929.scm
 
 echo Running gpgsm tests...
@@ -30,7 +30,7 @@ mkdir gpgsm
 cd gpgsm
 
 set GPGSCM_PATH=%BIN_PREFIX%/tests/gpgscm;%BIN_PREFIX%/tests/openpgp;%BIN_PREFIX%/tests/gpgsm
-set srcdir=%BIN_PREFIX%/tests/gpgsm
+set abs_top_srcdir=%BIN_PREFIX%/tests/gpgsm
 %BIN_PREFIX%\gpgscm.exe %srcdir%/run-tests.scm --parallel import.scm encrypt.scm verify.scm decrypt.scm sign.scm export.scm
 
 echo Running GPGME tests...
@@ -40,7 +40,7 @@ cd gpgme
 
 rem set verbose=3
 set GPGSCM_PATH=%BIN_PREFIX%/tests/gpgscm;%BIN_PREFIX%/tests/openpgp;%BIN_PREFIX%/tests/gpgme
-set srcdir=%BIN_PREFIX%/tests/gpgme
+set abs_top_srcdir=%BIN_PREFIX%/tests/gpgme
 set XTEST_GPGME_SRCDIR=%BIN_PREFIX%/gpgme
 set XTEST_GPGME_BUILDDIR=%BIN_PREFIX%/gpgme/obj.w32
 %BIN_PREFIX%\gpgscm.exe %srcdir%/run-tests.scm --parallel
